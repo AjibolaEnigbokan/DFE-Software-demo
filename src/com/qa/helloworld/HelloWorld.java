8b914a97eca4fc94ebb86c11d10bb2b6870dd365
BolaEnigbokan <jibolaenny@yahoo.com>
2022-06-14 10:17:11 +0100

    Ajibola mesage

---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.qa.helloworld;
+
+public class HelloWorld {
+	
+	
+	
+	public static void main(String[] args) {
+//		
+		printMessage();
+		
+	}
+
+
+public static void printMessage() {
+//	 DataType variablName equals value
+	
+	String myMessage = " Hello World";
+	
+	System.out.println(myMessage);
+		
+	}
+
+}
+
+
+
+
+
+
